108b4b8ebc22d6df857f1bfa6949e683340b7f1a
mbolton <sanctarua@gmail.com>
2014-05-16 11:51:54 -0400

    Added Spaceship Bitmap

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
-#include <allegro.h>
-	
+#include <allegro.h>	
 int main(){
 	 
 	BITMAP *background;
-	
+	BITMAP *ship;
+	BITMAP *buffer;
+	int shipX;
+	int shipY;
+
 	allegro_init();
 	install_keyboard();
 
 	/*** Creates a window with the set background ***/
-	set_color_depth(desktop_color_depth());
+	set_color_depth(32);
 	if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, 1280, 720, 0, 0) != 0)  // Attempts to set the graphic mode.
 	{
 		allegro_message("Error! Can not set graphic mode!"); 
@@ -21,10 +24,26 @@ int main(){
 		allegro_message("Error! Unable to load background!");
 		exit(0);
 	}
-	blit(background, screen, 0,0,0,0, background->w, background->h);
+
+	ship = load_bitmap("ship.bmp", NULL);
+	if (!ship)                                                        // Attempts to load the ship image.
+	{
+		allegro_message("Error! Unable to load ship!");
+		exit(0);
+	}
+	set_alpha_blender();
+	buffer = create_bitmap(SCREEN_W, SCREEN_H);
+	blit(background, buffer, 0,0,0,0, background->w, background->h);
+
+	shipX = SCREEN_W/2;
+	shipY = SCREEN_H/2;
+	draw_trans_sprite(buffer, ship, shipX, shipY);
+
+	acquire_screen();
+	blit(buffer, screen, 0,0,0,0, SCREEN_W, SCREEN_H);
+	release_screen();
 	
 	readkey();    
-	return 0;
-	     
+	return 0;     
 }   
 END_OF_MAIN();  
